e9b40dfd6f5643d1ef168ff5e7d5e114ecf8bbfe
uRedonkulous <kaitoyomma@yahoo.com>
2022-08-25 09:58:21 -0600

    Counter Assign

---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+
+app.secret_key="BlueShoes."
+
+
+@app.route('/')
+def index():
+    if "count" not in session:
+        session ["count"] = 0
+    else:
+        session['count'] += 1
+    return render_template("index.html")
+
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect('/')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
