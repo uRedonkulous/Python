c7ae05c998b5bb822db83ce81d43aa227c076bda
uRedonkulous <kaitoyomma@yahoo.com>
2023-03-23 22:49:39 -0600

    Changes

--- Freelance/Planner/flask_app/controllers/activities.py
+++ Freelance/Planner/flask_app/controllers/activities.py
@@ -12,8 +12,9 @@ def activities_dash():
         flash("You must be logged in to view page")
         return redirect("/")
     user = User.get_by_id(session["user_id"])
-    # activities= Activity.get_all()
-    return render_template("dashboard.html", user=user)
+    activities = Activity.get_all()
+    activity = Activity.get_all_by_user(session["user_id"])
+    return render_template("dashboard.html", user=user, activities=activities, activity=activity)
 
 #  Get activity
 @app.route('/activities/<int:activity_int>')
@@ -22,21 +23,53 @@ def activity_desc(activity_id):
     activity=Activity.get_by_id(activity_id)
     return render_template('activity_desc.html',user=user,activity=activity)
 
-#  Create activity
-@app.route('/activities/create', methods=['GET','POST'])
+#  Creating a Activity POST to form to INSERT into DB
+@app.route('/activities/create')
 def create_activity():
-    if request.method == "POST":
-    
-        valid_activity=Activity.create_valid_activity(request.form)
-        if valid_activity:
-            return redirect('/activities/dashboard')
-    return render_template('create_activity.html')
+    if "user_id" not in session:
+        return redirect('/')
+    user = User.get_by_id(session["user_id"])
+    return render_template("create_activity.html", user=user)
+
+#  Create activity Validation
+@app.route('/activities',methods=["POST"])
+def create_activities():
+    activity = {
+        "activity_name": request.form["activity_name"],
+        "duration":request.form["duration"], 
+        "description":request.form["description"], 
+        "user_id":session["user_id"]
+    }
+    valid_activity=Activity.create_valid_activity(activity)
+    if valid_activity:
+        return redirect('/activities/dashboard')
+    return redirect('/activities/create')
 
 #  Edit activity
 @app.route('/activities/edit/<int:activity_id>')
 def edit_activity(activity_id):
+    if "user_id" not in session:
+        return redirect('/')
     activity=Activity.get_by_id(activity_id)
-    return render_template('/edit_activity.html', activity=activity)
+    user = User.get_by_id(session["user_id"])
+    valid_activity=Activity.is_valid
+    if valid_activity:
+        return render_template('/edit_activity.html', user=user, activity=activity)
+    return ('/activities/edit/<int:activity_id>')
+
+# #  Update Activity
+@app.route("/activities/<int:activity_id>", methods=["POST"])
+def update_activity(activity_id):
+    activity = {
+        "id" : activity_id,
+        "activity_name" : request.form["activity_name"],
+        "duration" : request.form["duration"],
+        "description" : request.form["description"]
+    }
+    valid_activity = Activity.update_activity(activity, session["user_id"])
+    if not valid_activity:
+        return redirect(f"/activities/edit/{activity_id}")
+    return redirect(f"/activities/dashboard")
 
 #  Delete activity
 @app.route('/activities/delete/<int:activity_id>')
